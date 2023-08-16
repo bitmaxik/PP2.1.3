a7ac145ee34dba483ddd3177906cc046b9bacc4a
bitmaxik <bitmaxik@gmail.com>
2023-08-16 16:21:59 +0300

    add PP2.1.3

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -7,6 +7,19 @@ public class App {
                 new AnnotationConfigApplicationContext(AppConfig.class);
         HelloWorld bean =
                 (HelloWorld) applicationContext.getBean("helloworld");
+        HelloWorld bean1 =
+                (HelloWorld) applicationContext.getBean("helloworld");
         System.out.println(bean.getMessage());
+        Cat cat =
+                (Cat) applicationContext.getBean("cat");
+        cat.setName("Barsik");
+        Cat cat1 =
+                (Cat) applicationContext.getBean("cat");
+        cat1.setName("Murzik");
+
+        System.out.println("Объекты класса HelloWorld равны? " + (bean == bean1));
+        System.out.println("Объекты класса Cat равны? " + (cat == cat1));
+        System.out.println("Имя первого кота " + cat.getName());
+        System.out.println("Имя второго кота " + cat1.getName());
     }
 }
